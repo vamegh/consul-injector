e7859e178474811a57017364cc2d3bd98b707199
Vamegh Hedayati <vhedayati@ev9.io>
2018-04-09 03:41:04 +0100

    fixing naming

--- vstdlib/sorter.py
+++ vstdlib/sorter.py
@@ -18,15 +18,15 @@ class DataSort(object):
     def __init__(self):
         self.sorted_data = {}
 
-    def iterator(self, key='', value=None):
+    def recursor(self, key='', value=None):
         if isinstance(value, dict):
             for nk, nv in value.items():
                 fk = "{}/{}".format(key, nk)
-                self.iterator(key=fk, value=nv)
+                self.recursor(key=fk, value=nv)
         elif isinstance(value, list):
             for i, nv in enumerate(value):
                 fk = "{}[{}]".format(key, i)
-                self.iterator(key=fk, value=nv)
+                self.recursor(key=fk, value=nv)
         else:
             self.sorted_data[key] = value
         return self.sorted_data
